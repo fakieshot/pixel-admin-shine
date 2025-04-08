77ad9f5b1d13f65ed049af769b881701b9c7a562
ARTEMIOU STAVROS <sartemiou@iekakmi.edu.gr>
2025-04-08 12:30:20 +0300

    Sync local changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCeF4eUPyOigUuqvYSkaCYehJobfWK9okE",
+  authDomain: "filoxenia-35136.firebaseapp.com",
+  projectId: "filoxenia-35136",
+  storageBucket: "filoxenia-35136.firebasestorage.app",
+  messagingSenderId: "373641445474",
+  appId: "1:373641445474:web:1e1d34509dd196f147513b",
+  measurementId: "G-WV3XXPV8H2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
