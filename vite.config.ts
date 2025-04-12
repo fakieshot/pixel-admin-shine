bd546ebc90801bc45216c32588d3a8d64a735c52
ARTEMIOU STAVROS <sartemiou@iekakmi.edu.gr>
2025-04-12 19:18:27 +0300

    test final

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,17 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // 
+  base: "/", // Leave as is unless you're using subpaths
   server: {
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: "dist", // <-- Add this for Netlify to know where to publish
+  },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
